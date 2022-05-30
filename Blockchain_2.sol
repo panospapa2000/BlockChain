3bb23b1f19ab111f68b5ef736a377280ddd46fb0
"BlockChain" <"pl4ym0bil3@hotmail.com">
2022-05-30 17:30:58 +0300

    modifier(onlyOwner)

--- Blockchain_2.sol
+++ Blockchain_2.sol
@@ -16,25 +16,28 @@ contract Lottery
         uint itemId;
         uint itemTokens;
     }
+
     Item [3] public items;
     
     address [3] public winners;
     address public beneficiary;
 
-    uint bidderAccount = 0;
+    uint bidderCount = 0;
 
     constructor() public payable
     {
         beneficiary = msg.sender;
         uint[] memory emptyArray;
-        items[0] = Item({itemId:0, itemTokens:emptyArray});
-        items[1] = Item({itemId:1, itemTokens:emptyArray});
-        items[2] = Item({itemId:2, itemTokens:emptyArray});
+
+        items[0] = Item({itemId:0, itemTokens:0});
+        items[1] = Item({itemId:1, itemTokens:1});
+        items[2] = Item({itemId:2, itemTokens:2});
     }
 
     function register() public payable
     {
         bidders[bidderCount].personId = bidderCount;
+
         bidders[bidderCount].addr = msg.sender;
         bidders[bidderCount].remainingTokens = 5;
         tokenDetails[msg.sender] = bidders[bidderCount];
@@ -49,6 +52,12 @@ contract Lottery
     function revealWinners() public onlyOwner
     {
 
+    }
+
+    modifier onlyOwner//Modifier gia ton beneficiary
+    {
+        require(msg.sender == beneficiary);
+        _;
     }
 
     function getPersonDetails(uint id) public view returns(uint, uint, address)
